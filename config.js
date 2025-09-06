491c941c0772b045e598347dd95a81aefe8b0e48
taffy367m <marikasishayne@gmail.com>
2025-09-06 22:01:01 +0200

    Update config.js for xylo-xy2k1g

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const defaults = {
 
 let cache = {}
 
-const SESSION_ID = process.env.SESSION_ID || ''
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=89B7JA3SNL'
 cache.SESSION_ID = SESSION_ID 
 
 async function initConfig() {
