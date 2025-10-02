941cb9fe80da9c18ffd5bfd7e3d48ac60334dd6c
taffy367m <marikasishayne@gmail.com>
2025-10-02 14:18:34 +0200

    Update config.js for xylo-t12t6n

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const defaults = {
 
 let cache = {}
 
-const SESSION_ID = process.env.SESSION_ID || ''
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=CEVT4KB90T'
 cache.SESSION_ID = SESSION_ID 
 
 async function initConfig() {
